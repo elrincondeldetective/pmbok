43d64937e2af80a687c160b818709babb5ba5b1e
Erik Lemus <reydem613@gmail.com>
2025-09-24 07:21:20 -0500

    feat(cors): configura el middleware y los orígenes permitidos
    Se integra y configura `django-cors-headers` para permitir la comunicación entre el frontend y el backend durante el desarrollo.
    
    Se añade `corsheaders.middleware.CorsMiddleware` a la lista de `MIDDLEWARE` para que intercepte las peticiones y añada las cabeceras CORS necesarias. Además, se define la variable `CORS_ALLOWED_ORIGINS` para autorizar explícitamente las peticiones provenientes del servidor de desarrollo de React (`http://localhost:3000`).
    
    Conteo: commit #4

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -50,11 +50,18 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# Orígenes permitidos (tu app de React en desarrollo)
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 ROOT_URLCONF = 'core.urls'
